4fa616d0465d130082d057399b766bcfd6843659
Adán Luis Quincoces Escalona <adanlqe@gmail.com>
2025-12-23 23:21:54 -0500

    Add Web Icon, fix Label "Player.tsx" and config gh-pages

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -35,13 +35,40 @@ export function Player() {
 
       {/* Imagen o Cover de la Canción */}
       <div className='flex justify-center items-center'>
-        <CoverSongs artist={currentSong.artist} name={currentSong.name} variant='player'/>
+        <CoverSongs artist={currentSong.artist} name={currentSong.name} variant='player' />
       </div>
 
       {/* Nombre de la canción y Artista */}
       <div className='my-2 text-center'>
         <h1 className='text-xl h-7'>{currentSong.artist ? currentSong.artist : 'Seleccione una canción.'}</h1>
-        <h2 className='text-3xl h-9'>{currentSong.name ? currentSong.name : <span className='flex gap-3'><div className='flex'><p className='m-0 text-red-600'>A</p><p className='m-0 text-yellow-600'>u</p><p className='m-0 text-blue-600'>d</p><p className='m-0 text-orange-600'>i</p><p className='m-0 text-pink-600'>o</p><p className='m-0 text-purple-600'>-</p><p className='m-0 text-red-600'>P</p><p className='m-0 text-yellow-600'>l</p><p className='m-0 text-blue-600'>a</p><p className='m-0 text-orange-600'>y</p><p className='m-0 text-pink-600'>e</p><p className='m-0 text-purple-600'>r</p><p className='m-0 text-red-600'> </p><p className='m-0 text-yellow-600'>W</p><p className='m-0 text-blue-600'>e</p><p className='m-0 text-orange-600'>b</p></div><div className='flex'><p className='m-0'>v</p><p className='m-0 text-'>1</p><p className='m-0 text-'>.</p><p className='m-0 text-'>0</p></div></span>}</h2>
+        <h2 className='text-3xl h-9'>{currentSong.name ? currentSong.name :
+          <span className='flex gap-3'>
+            <div className='flex'>
+              <p className='m-0 text-red-600'>A</p>
+              <p className='m-0 text-yellow-600'>u</p>
+              <p className='m-0 text-blue-600'>d</p>
+              <p className='m-0 text-orange-600'>i</p>
+              <p className='m-0 text-pink-600'>o</p>
+              <p className='m-0 text-purple-600'>-</p>
+              <p className='m-0 text-red-600'>P</p>
+              <p className='m-0 text-yellow-600'>l</p>
+              <p className='m-0 text-blue-600'>a</p>
+              <p className='m-0 text-orange-600'>y</p>
+              <p className='m-0 text-pink-600'>e</p>
+              <p className='m-0 text-purple-600'>r</p>
+            </div>
+            <div className='flex'>
+              <p className='m-0 text-yellow-600'>W</p>
+              <p className='m-0 text-blue-600'>e</p>
+              <p className='m-0 text-orange-600'>b</p>
+            </div>
+            <div className='flex'>
+              <p className='m-0'>v</p>
+              <p className='m-0 text-'>1</p>
+              <p className='m-0 text-'>.</p>
+              <p className='m-0 text-'>0</p>
+            </div>
+          </span>}</h2>
       </div>
 
       {/* Tiempo transcurrido, slider y Tiempo total del audio */}
